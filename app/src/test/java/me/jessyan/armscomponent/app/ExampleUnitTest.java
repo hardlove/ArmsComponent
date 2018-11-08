c73550bccfb60796a3b10682fbcb6e5a41d244f4
CL <413162035@qq.com>
2018-11-08 14:20:06 +0800

    添加多状态控件，empty、error、content、loading

--- app/src/test/java/me/jessyan/armscomponent/app/ExampleUnitTest.java
+++ app/src/test/java/me/jessyan/armscomponent/app/ExampleUnitTest.java
@@ -8,9 +8,21 @@ import static org.junit.Assert.assertEquals;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void tesView() {
+        int childCount = 5;
+        int index = 0;
+        for (int i = 0; i < childCount; i++) {
+            index = i;
+            break;
+        }
+        assertEquals(1, index);
     }
 
 }
