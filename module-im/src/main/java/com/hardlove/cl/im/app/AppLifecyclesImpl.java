ff4d85ad25bacd0a34b06b1890e0f3fdfd442060
CL <413162035@qq.com>
2018-11-21 16:35:30 +0800

    增加高德so库，融云消息推送

--- module-im/src/main/java/com/hardlove/cl/im/app/AppLifecyclesImpl.java
+++ module-im/src/main/java/com/hardlove/cl/im/app/AppLifecyclesImpl.java
@@ -13,6 +13,8 @@ import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
 import io.rong.imkit.RongIM;
+import io.rong.imlib.model.Group;
+import io.rong.imlib.model.UserInfo;
 import me.jessyan.armscomponent.commonsdk.utils.ProcessUtils;
 
 /**
@@ -24,7 +26,7 @@ import me.jessyan.armscomponent.commonsdk.utils.ProcessUtils;
  * <a href="https://github.com/JessYanCoding">Follow me</a>
  * ================================================
  */
-public class AppLifecyclesImpl implements AppLifecycles {
+public class AppLifecyclesImpl implements AppLifecycles{
 
     @Override
     public void attachBaseContext(@NonNull Context base) {
@@ -39,9 +41,8 @@ public class AppLifecyclesImpl implements AppLifecycles {
     @Override
     public void onCreate(@NonNull Application application) {
         if (ProcessUtils.isMainProcess(application)) {
-            //初始化融云IM SDK
-            RongIM.init(application);
-            Log.d("初始化", "初始化融云IM SDK");
+            initRongIM(application);
+
         }
         if (LeakCanary.isInAnalyzerProcess(application)) {
             // This process is dedicated to LeakCanary for heap analysis.
@@ -54,6 +55,30 @@ public class AppLifecyclesImpl implements AppLifecycles {
             ArmsUtils.obtainAppComponentFromContext(application).extras().put(RefWatcher.class.getName(), BuildConfig.USE_CANARY ? LeakCanary.install(application) : RefWatcher.DISABLED);
         }
 
+    }
+
+    private void initRongIM(@NonNull Application application) {
+        //初始化融云IM SDK
+        RongIM.init(application);
+        Log.d("初始化", "初始化融云IM SDK");
+        RongIM.setUserInfoProvider(new RongIM.UserInfoProvider() {
+            @Override
+            public UserInfo getUserInfo(String s) {
+                return null;
+            }
+        }, true);
+        RongIM.setGroupInfoProvider(new RongIM.GroupInfoProvider() {
+            @Override
+            public Group getGroupInfo(String s) {
+                return null;
+            }
+        }, true);
+        RongIM.setLocationProvider(new RongIM.LocationProvider() {
+            @Override
+            public void onStartLocation(Context context, LocationCallback locationCallback) {
+
+            }
+        });//设置地理位置提供者,不用位置的同学可以注掉此行代码
     }
 
     @Override
