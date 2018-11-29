828e721a836e1e826f302bd9d27d362fdb8a26bc
CL <413162035@qq.com>
2018-11-29 12:27:41 +0800

    ExampleUnitTest.java AndroidManifest.xml base_values.xml 修改尺寸

--- module-defender/src/test/java/com/hardlove/cl/fooddefender/ExampleUnitTest.java
+++ module-defender/src/test/java/com/hardlove/cl/fooddefender/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package com.hardlove.cl.fooddefender;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +18,41 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void gentatory() {
+        int x = 1080;
+        int y = 1920;
+        int f = 100;
+        File file = new File("base_values.xml");
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            StringBuilder sb = new StringBuilder();
+            String tmpx = "<dimen name=\"x_ui_px_%s\">%smm</dimen>";
+            String tmpy = "<dimen name=\"y_ui_px_%s\">%smm</dimen>";
+            String tmpf = "<dimen name=\"font_ui_px_%s\">%smm</dimen>";
+            sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+                    .append("\n")
+                    .append("<resources>").append("\n");
+            for (int i = 0; i < x; i++) {
+                sb.append(String.format(tmpx, i + ".0", i + ".0")).append("\n");
+            }
+            for (int i = 0; i < y; i++) {
+                sb.append(String.format(tmpy, i + ".0", i + ".0")).append("\n");
+            }
+            for (int i = 0; i < f; i++) {
+                sb.append(String.format(tmpf, i + ".0", i + ".0")).append("\n");
+            }
+            sb.append("</resources>");
+
+            fos.write(sb.toString().getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
